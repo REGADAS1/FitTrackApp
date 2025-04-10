b00fd41de1e237e1bb98e906b8dfa5281845668d
Goncalo Regadas <regadas02@gmail.com>
2025-04-10 12:25:51 +0100

    User Collection Created

--- lib/data/sources/auth/auth_firebase_service.dart
+++ lib/data/sources/auth/auth_firebase_service.dart
@@ -1,42 +1,62 @@
 import 'package:dartz/dartz.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:fit_track_app/data/models/auth/create_user_req.dart';
 
 abstract class AuthFirebaseService {
-  Future<Either> signup(CreateUserReq createUserReq);
+  Future<Either<String, String>> signup(CreateUserReq createUserReq);
   Future<void> signin();
 }
 
 class AuthFirebaseServiceImpl extends AuthFirebaseService {
   @override
-  Future<Either> signup(CreateUserReq createUserReq) async {
+  Future<Either<String, String>> signup(CreateUserReq createUserReq) async {
     try {
-      // Simulate a network call
-      await FirebaseAuth.instance.createUserWithEmailAndPassword(
-        email: createUserReq.email,
-        password: createUserReq.password,
-      );
-      return Right('User signed up successfully'); // Return success
+      // Criar utilizador no Firebase Authentication
+      UserCredential userCredential = await FirebaseAuth.instance
+          .createUserWithEmailAndPassword(
+            email: createUserReq.email,
+            password: createUserReq.password,
+          );
+
+      // Obter UID do utilizador
+      final uid = userCredential.user?.uid;
+
+      if (uid == null) {
+        return Left('Erro ao obter UID do utilizador');
+      }
+
+      // Criar documento no Firestore (coleção "users")
+      await FirebaseFirestore.instance.collection('users').doc(uid).set({
+        'firstName': createUserReq.name,
+        'lastName': createUserReq.lastname,
+        'email': createUserReq.email,
+        'createdAt': Timestamp.now(),
+      });
+
+      return Right('Utilizador registado com sucesso');
     } on FirebaseAuthException catch (e) {
       String message = '';
       switch (e.code) {
         case 'email-already-in-use':
-          message = 'Email already in use';
+          message = 'Email já está em uso';
           break;
         case 'invalid-email':
-          message = 'Invalid email address';
+          message = 'Email inválido';
           break;
         case 'operation-not-allowed':
-          message = 'Operation not allowed';
+          message = 'Operação não permitida';
           break;
         case 'weak-password':
-          message = 'Weak password';
+          message = 'Password fraca';
           break;
         default:
-          message = 'An unknown error occurred';
+          message = 'Ocorreu um erro desconhecido';
       }
 
       return Left(message);
+    } catch (e) {
+      return Left('Erro inesperado: ${e.toString()}');
     }
   }
 
